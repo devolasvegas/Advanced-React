a9fe0a4d07234cd1163d5aa6da6cf2f34e431587
Devon Daviau <devondaviau@yahoo.ca>
2025-04-23 10:07:17 -0400

    in the frontend, create the cart

---
+++ sick-fits/frontend/components/Cart.js
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import CartStyles from './styles/CartStyles';
+import Supreme from './styles/Supreme';
+
+import { useUser } from './User';
+
+import formatMoney from '../lib/formatMoney';
+import calcTotalPrice from '../lib/calcTotalPrice';
+
+const CartItemStyles = styled.li`
+  padding: 1rem 0;
+  border-bottom: 1px solid var(--lightGrey);
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  img {
+    margin-right: 1rem;
+  }
+  h4,
+  p {
+    margin: 0;
+  }
+`;
+
+function CartItem({ cartItem }) {
+  const { product } = cartItem;
+
+  return (
+    <CartItemStyles>
+      <img
+        src={product.photo.image.publicUrlTransformed}
+        alt={product.photo.altText ?? product.name ?? ''}
+        width="100"
+      />
+      <div>
+        <h3>{product.name}</h3>
+        <p>
+          {formatMoney(product.price * cartItem.quantity)} –{' '}
+          <em>
+            {cartItem.quantity} &times; {formatMoney(product.price)} each
+          </em>
+        </p>
+      </div>
+    </CartItemStyles>
+  );
+}
+
+CartItem.propTypes = {
+  cartItem: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    product: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      photo: PropTypes.shape({
+        image: PropTypes.shape({
+          publicUrlTransformed: PropTypes.string.isRequired,
+        }).isRequired,
+        altText: PropTypes.string,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default function Cart() {
+  const me = useUser();
+
+  if (!me) return null;
+
+  return (
+    <CartStyles open>
+      <header>
+        <Supreme>{me.name}'s Cart</Supreme>
+      </header>
+      <ul>
+        {me.cart.map((cartItem) => (
+          <CartItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+      </ul>
+      <footer>
+        <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+      </footer>
+    </CartStyles>
+  );
+}
--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import styled from 'styled-components';
 
 import Nav from './Nav';
+import Cart from './Cart';
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -45,6 +46,7 @@ export default function Header() {
         <Nav />
       </div>
       <div className="sub-bar">Search</div>
+      <Cart />
     </HeaderStyles>
   );
 }
